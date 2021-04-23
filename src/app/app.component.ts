bd4e3417170663495978ec1256a265523f22778d
dharmendrajaitley <dharmendrajaitley@gmail.com>
2021-04-23 08:41:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { WindowService } from './common/window.service';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'firebase-demo';
+
+  phoneNumber:any;
+  otp:any;
+  windowRef:any;
+  user:any;
+
+  constructor(private windowService:WindowService ){
+    
+  }
+
+  ngOnInit(){
+    this.windowRef=this.windowService.windowRef;
+    this.windowRef.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('captcha-container')
+    this.windowRef.recaptchaVerifier.render()
+    
+  }
+
+  
+
+  sendOtp()
+  {
+    const appVerifier=this.windowRef.recaptchaVerifier;
+    firebase.auth().signInWithPhoneNumber('+91'+ this.phoneNumber,appVerifier)
+    .then(result =>{
+      this.windowRef.confirmationResult=result;
+    })
+    .catch(error => console.log(error));
+  }
+
+  verifyOtp()
+  {
+    this.windowRef.confirmationResult
+                  .confirm(this.otp)
+                  .then(result =>{
+                    this.user=result.user;
+                    console.log(this.user)
+                  })
+                  .catch( error =>console.log(error, 'Incorrect code entered'))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import * as firebase from 'firebase';
 
+import {WindowService} from './common/window.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAtfG7wAs7UniUiFeRbxnKxFYQi-RTU-lM",
+  authDomain: "fir-demo-4b4cd.firebaseapp.com",
+  projectId: "fir-demo-4b4cd",
+  storageBucket: "fir-demo-4b4cd.appspot.com",
+  messagingSenderId: "540324460581",
+  appId: "1:540324460581:web:75772fd655d3827431a7cc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +28,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    
   ],
-  providers: [],
+  providers: [WindowService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
